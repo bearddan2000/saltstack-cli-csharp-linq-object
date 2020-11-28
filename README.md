1224092d680790fbff07793db599a2502cb9e08b
bearddan2000 <67988550+bearddan2000@users.noreply.github.com>
2020-11-27 21:57:05 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,30 @@
+# saltstack-cli-csharp-linq-object
+
+## Description
+A c# linq demo that uses an object
+type collection as input.
+Built by salt.
+
+## Tech stack
+- bash
+- c#
+  - Framework 4.7
+- mono 6.8
+- salt
+
+## Docker stack
+- docker-cli
+- ubuntu:20.04
+
+## Requirements
+Docker desktop must be installed and the application
+being called must be linux compatiple.
+
+## To run
+`sudo ./install.sh -u`
+
+## To stop (optional)
+`sudo ./install.sh -d`
+
+## To see help
+`sudo ./install.sh -h`
---
+++ bin/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace MyAppCshrp
+{
+    class Person
+    {
+        public String Name { get; set; }
+        public int Age { get; set; }
+
+        public override String ToString()
+        {
+            return String.Format("Name: {0}, Age: {1}", Name, Age.ToString("D2"));
+        }
+    }
+    class MainClass
+    {
+        private static void print(IEnumerable<Person> result, String query)
+        {
+            Console.WriteLine("[QUERY] {0}", query);
+            foreach (Person i in result)
+                Console.WriteLine("[OUTPUT] {0}", i);
+        }
+        private static void greaterThan(List<Person> list)
+        {
+            var result = (from x in list where x.Age > 1 select x);
+            print(result, "SELECT * FROM list WHERE age > 1;");
+        }
+        private static void lessThan(List<Person> list)
+        {
+            var result = (from x in list where x.Age < 2 select x);
+            print(result, "SELECT * FROM list WHERE age < 2;");
+        }
+        private static void equals(List<Person> list)
+        {
+            var result = (from x in list where x.Age == 1 select x);
+            print(result, "SELECT * FROM list WHERE age = 1;");
+        }
+        public static void Main(string[] args)
+        {
+            List<Person> list = new List<Person>() {
+                        new Person(){Name="Adam", Age=0},
+                        new Person(){Name="Betty", Age=1},
+                        new Person(){Name="Charlie", Age=2}
+            };
+            Console.WriteLine("[INPUT] {0}", String.Join<Person>(", ", list.ToArray()));
+            greaterThan(list);
+            lessThan(list);
+            equals(list);
+        }
+    }
+}
